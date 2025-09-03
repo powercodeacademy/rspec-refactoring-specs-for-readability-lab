198b3d59cddfba449ecc98126404635cab067640
Jen Kelly <jen.kelly@powerhrg.com>
2025-09-03 16:00:02 -0400

    Refactor multiple run code

--- spec/student/animal_spec.rb
+++ spec/student/animal_spec.rb
@@ -4,44 +4,36 @@ describe Animal do
 
   context 'when animal is a dog' do
     let(:animal) { Animal.new("Dog", 3, "Dog") }
-    it "returns the correct name for a dog both times" do
-      2.times do
+      
+    2.times do   
+      it "returns the correct name for a dog both times" do
         expect(animal.name).to eq("Dog")
       end
-    end
 
-    it "returns the correct age for a dog both times" do
-      2.times do
-        expect(animal.age).to eq(3)
+      it "returns the correct age for a dog both times" do
+          expect(animal.age).to eq(3)
+      end
+
+      it "speaks correctly for a dog both times" do
+          expect(animal.speak).to eq("Woof!")
       end
     end
-
-    it "speaks correctly for a dog both times" do
-      2.times do
-        expect(animal.speak).to eq("Woof!")
-      end
-    end
-
   end
   
 
   context 'when animal is a cat' do  
     let(:animal) { Animal.new("Cat", 2, "Cat") }
-    it "returns the correct name for a cat both times" do
-      2.times do
+    2.times do
+      it "returns the correct name for a cat both times" do
         expect(animal.name).to eq("Cat")
       end
-    end
 
-    it "returns the correct age for a cat both times" do
-      2.times do
-        expect(animal.age).to eq(2)
+      it "returns the correct age for a cat both times" do
+          expect(animal.age).to eq(2)
       end
-    end
 
-    it "speaks correctly for a cat both times" do
-      2.times do
-        expect(animal.speak).to eq("Meow!")
+      it "speaks correctly for a cat both times" do
+          expect(animal.speak).to eq("Meow!")
       end
     end
   end
